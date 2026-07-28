df0ca2ad98e2da5edee8fc8be9e9992721a01573
Marc Pulido <marc.pulido@gmail.com>
2026-07-27 21:02:45 -0400

    adding training

---
+++ auto_cut.sh
@@ -0,0 +1,9 @@
+
+
+# crop to cut by width x height, x,y coordinates is upper left corner
+#
+# convert input.png -crop 800x600+100+50 +repage output.png
+# for img in *.png; do
+#     convert "$img" -crop 800x600+100+50 +repage "cut_$img"
+# done
+
